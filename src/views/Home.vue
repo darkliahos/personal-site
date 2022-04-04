fb9f130033aa898c77e664d94c62024e3993a356
Sohail Nasir <sohail@aventusplatform.com>
2022-04-04 13:28:13 +0100

    #10 Removed slide controls

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,10 @@
     <b-carousel
       id="carousel-1"
       v-model="slide"
-      :interval="4000"
-      controls
+      :interval="3500"
+      fade
       indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
+      background="#131313"
     >
       <!-- Slides with custom text -->
       <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=0">
